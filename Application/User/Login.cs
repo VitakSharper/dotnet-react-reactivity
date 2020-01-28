7a9d85b3ea58cd6e20e9d7993d28b73f0dc56f0f
VitakSharper <v.banard@gmail.com>
2020-01-28 15:01:27 +0100

    DOTNET:IDENTITY add user class to filter responses from api;
    refactor controller using new User.cs;

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -11,7 +11,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -25,7 +25,7 @@ namespace Application.User
                 RuleFor(x => x.Password).NotEmpty();
             }
         }
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
@@ -36,7 +36,7 @@ namespace Application.User
                 _signInManager = signInManager;
             }
 
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
@@ -50,7 +50,7 @@ namespace Application.User
                 if (result.Succeeded)
                 {
                     // TODO: generate token
-                    return user;
+                    return new User { DisplayName = user.DisplayName, Username = user.UserName, Token = "This will be a token.", Image = null };
                 }
                 throw new RestException(HttpStatusCode.Unauthorized);
             }
