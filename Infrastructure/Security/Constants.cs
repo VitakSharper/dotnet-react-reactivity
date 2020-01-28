e0331b4f3aed72512fb2d169e2bd3e50734a517c
VitakSharper <v.banard@gmail.com>
2020-01-28 16:37:24 +0100

    DOTNET:Jwt add CreateToken method in JwtGenerator; implements jwtgenerator interface in startup.cs;
    move to Constants.cs some values;

---
+++ Infrastructure/Security/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Security
+{
+    public class Constants
+    {
+        public const string JwtKey = "KQQ38EV5visH5Bh88w3jzSe65Te3V22FrbpjGN7yH4";
+        public const int JwtExpires = 7;
+    }
+}
