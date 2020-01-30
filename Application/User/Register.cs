f9d77f8e807f3187ea44716f210f62896310d1bd
VitakSharper <v.banard@gmail.com>
2020-01-30 15:31:45 +0100

    DOTNET:Regiser Add Register.cs Handler

---
+++ Application/User/Register.cs
@@ -0,0 +1,80 @@
+﻿using Application.Errors;
+using Application.Interfaces;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    public class Register
+    {
+        public class Command : IRequest<User>
+        {
+            public string DisplayName { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+                RuleFor(x => x.UserName).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+
+            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
+            {
+                _context = context;
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+                    throw new RestException(HttpStatusCode.BadRequest, new { Email = "Email already exists." });
+
+                if (await _context.Users.AnyAsync(u => u.UserName == request.UserName))
+                    throw new RestException(HttpStatusCode.BadRequest, new { UserName = "Username already exists." });
+
+                var user = new AppUser
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.UserName
+                };
+
+                // handler logic  
+                var result = await _userManager.CreateAsync(user, request.Password);
+                if (result.Succeeded)
+                {
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        Username = user.UserName,
+                        Image = null
+                    };
+                }
+                throw new Exception("Problem creating user.");
+            }
+        }
+    }
+}
