757e795a26428d0c0f7d658978a00cc2b330c101
VitakSharper <v.banard@gmail.com>
2020-02-24 10:17:40 +0100

    DOTNET:SignalR add comment entity for the chat functionality

---
+++ Domain/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace Domain
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Body { get; set; }
+        public virtual AppUser Author { get; set; }
+        public virtual Activity Activity { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
